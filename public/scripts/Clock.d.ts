aa3877b4a7478d4b211300505b1ad5842d9326eb
danillque <danieel.gusev@gmail.com>
2021-05-01 17:59:49 +0300

    second commit

--- public/scripts/Clock.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class Clock {
-    timeOutput: HTMLElement;
-    UTCplus: number;
-    socket: WebSocket;
-    constructor(element: HTMLElement);
-    _handleSocket(event: MessageEvent): void;
-    run(): void;
-}
--- public/scripts/Greeting.d.ts
+++
@@ -1,7 +0,0 @@
-export declare class Greeting {
-    greetOutput: HTMLElement;
-    socket: WebSocket;
-    constructor(element: HTMLElement);
-    _handleSocket(event: MessageEvent): void;
-    run(): void;
-}
